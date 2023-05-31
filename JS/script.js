4b76723da63d4fb0278beebdf5ac492a0799b260
ავთანდილ საგინაშვილი <120257059+AvtandilSaginashvili@users.noreply.github.com>
2023-05-31 20:05:33 +0400

    update

--- JS/script.js
+++ JS/script.js
@@ -5,9 +5,9 @@ function login1(){
     var paroli = document.getElementById("pswd").value;
     
 
-    if (meil.value == "" || meil.lenght == 0) {
+    if (meil == "" || meil.lenght == 0) {
         window.alert("ჩაწერილი ელფოსიტის ფორმატი არასწორია, გთხოვთ შეიყვანოთ ელფოსტა");
-    }else if (paroli.value == "" || paroli.lenght < 6) {
+    }else if (paroli == "" || paroli.lenght < 6) {
         window.alert("შეყვანილი მონაცემები არასწორია, გთხოვთ შეიყვანეთ პაროლი");
     }
 
